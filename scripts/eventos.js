388fbec2bbacc33fb0f7fcb7f0da0da3c65a1d93
jamartindev <jamarti94@hotmail.com>
2023-08-09 04:06:37 -0300

    event handler

--- scripts/eventos.js
+++ scripts/eventos.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const divboton = document.getElementById('div-btn');
+    const divboton = document.getElementById('div-btn');
+    const boton = document.getElementById('btn')
 
-divboton.addEventListener('click', function() {
-    alert('Hola! Soy el Div');
-});
+    divboton.addEventListener('click', function() {
+        alert('Hola! Soy el Div');
+        
+    });
+
+    boton.addEventListener('click', function(e) {
+        e.stopPropagation();
+        alert('Hola!');
+    });
 
 });

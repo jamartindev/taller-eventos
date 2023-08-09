f959c2883ae2de238605a7afcca72122d55363dc
jamartindev <jamarti94@hotmail.com>
2023-08-09 01:17:16 -0300

    Mensaje Hola! Soy el Div

---
+++ scripts/eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const divboton = document.getElementById('div-btn');
+
+divboton.addEventListener('click', function() {
+    alert('Hola! Soy el Div');
+});
+
+});
